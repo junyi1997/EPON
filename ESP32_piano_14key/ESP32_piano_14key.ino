943045cf870c4587eeeecba8daaab118872428e9
junyi1997 <q5896799@gmail.com>
2019-09-10 22:40:32 +0800

    '結案'

--- ESP32_piano_14key/ESP32_piano_14key.ino
+++ ESP32_piano_14key/ESP32_piano_14key.ino
@@ -1,7 +1,8 @@
 int pinADC[]={36, 39, 34, 35, 32, 33};//ADC1腳位
 int state[]={0,0,0,0,0,0,0,0,0,0,0,0,0,0};//狀態陣列
-int small_R=3700;//觸發區間<ADC_V0
-int big_R=1000;//觸發區間>ADC_V0
+int small_R=4000;//觸發區間<ADC_V0
+int small_R_ADC1=3700;//觸發區間<ADC_V0
+int big_R=100;//觸發區間>ADC_V0
 int ADC_V0[]={0,0,0,0,0,0,0,0,0,0,0,0,0,0};//取樣計數器
 int k=100;//取樣次數
 #include <driver/adc.h>
@@ -13,13 +14,16 @@ void setup()
 {
     for (int i=0; i<6; i++){pinMode(pinADC[i], ANALOG);}
     Serial.begin(115200);//序列視窗
-    SerialBT.begin("BLETEST");//<<藍芽名稱，請自己記得
+    SerialBT.begin("E_PON_Piano");//<<藍芽名稱，請自己記得
 }
 void loop()
 {
-  for(int j = 0 ; j < k ; j++){
-    for(int i = 0 ; i < 7 ; i++){ ADC_V0[i]+=analogRead(pinADC[i]);//讀取ADC01值}
-    //讀取ADC02值
+  for(int j = 0 ; j < k ; j++)
+  {
+    for(int i = 0 ; i < 7 ; i++)
+    {
+      ADC_V0[i]+=analogRead(pinADC[i]);
+    }
     int read_raw1;int read_raw2;int read_raw3;int read_raw4;
     int read_raw5;int read_raw6;int read_raw7;int read_raw8;
     adc2_config_channel_atten( ADC2_CHANNEL_8, ADC_ATTEN_6db );
@@ -38,6 +42,7 @@ void loop()
     esp_err_t r6 = adc2_get_raw( ADC2_CHANNEL_0, ADC_WIDTH_12Bit, &read_raw7); 
     adc2_config_channel_atten( ADC2_CHANNEL_3, ADC_ATTEN_6db );
     esp_err_t r7 = adc2_get_raw( ADC2_CHANNEL_3, ADC_WIDTH_12Bit, &read_raw8); 
+    
     ADC_V0[6]+=read_raw1;
     ADC_V0[7]+=read_raw2;
     ADC_V0[8]+=read_raw3;
@@ -46,9 +51,12 @@ void loop()
     ADC_V0[11]+=read_raw6;
     ADC_V0[12]+=read_raw7;
     ADC_V0[13]+=read_raw8;
-  }}
-    for (int i=0;i<14;i++){ADC_V0[i]=ADC_V0[i]/k;}//Serial.print(ADC_V0[i]);Serial.print("\t");
-  if (ADC_V0[0]<small_R && ADC_V0[0]>big_R){//Do0
+    
+  }
+    for (int i=0;i<14;i++){ADC_V0[i]=ADC_V0[i]/k;}//Serial.print(ADC_V0[i]);Serial.print("\t");}Serial.println();
+    
+    //delay(100);
+  if (ADC_V0[0]<small_R_ADC1 && ADC_V0[0]>big_R){//Do0
     if(state[0]==0){
       state[0]=1;
       Serial.println("Do0");SerialBT.print("Do0");
@@ -59,7 +67,7 @@ void loop()
       SerialBT.print("000");
     }
   }
-  if (ADC_V0[1]<small_R && ADC_V0[1]>big_R){//Re0
+  if (ADC_V0[1]<small_R_ADC1 && ADC_V0[1]>big_R){//Re0
     if(state[1]==0){
       state[1]=1;
       Serial.println("Re0");SerialBT.print("Re0");
@@ -70,7 +78,7 @@ void loop()
       SerialBT.print("000");
     }
   }
-  if (ADC_V0[2]<small_R && ADC_V0[2]>big_R){//Mi0
+  if (ADC_V0[2]<small_R_ADC1 && ADC_V0[2]>big_R){//Mi0
     if(state[2]==0){
       state[2]=1;
       Serial.println("Mi0");SerialBT.print("Mi0");
@@ -81,7 +89,7 @@ void loop()
       SerialBT.print("000");
     }
   }
-  if (ADC_V0[3]<small_R && ADC_V0[3]>big_R){//Fa0
+  if (ADC_V0[3]<small_R_ADC1 && ADC_V0[3]>big_R){//Fa0
     if(state[3]==0){
       state[3]=1;
       Serial.println("Fa0");SerialBT.print("Fa0");
@@ -92,7 +100,7 @@ void loop()
       SerialBT.print("000");
     }
   }
-  if (ADC_V0[4]<small_R && ADC_V0[4]>big_R){//So0
+  if (ADC_V0[4]<small_R_ADC1 && ADC_V0[4]>big_R){//So0
     if(state[4]==0){
       state[4]=1;
       Serial.println("So0");SerialBT.print("So0");
@@ -103,7 +111,7 @@ void loop()
       SerialBT.print("000");
     }
   }
-  if (ADC_V0[5]<small_R && ADC_V0[5]>big_R){//La0
+  if (ADC_V0[5]<small_R_ADC1 && ADC_V0[5]>big_R){//La0
     if(state[5]==0){
       state[5]=1;
       Serial.println("La0");SerialBT.print("La0");
@@ -202,5 +210,4 @@ void loop()
       SerialBT.print("000");
     }
   }
-     
 }
